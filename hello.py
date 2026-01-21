1f7dc975bfdefc693f2c06bf7f04287a606220f6
WANG-K-Alan <WZY18843828927@outlook.com>
2026-01-21 14:41:53 +0800

    hello

--- hello.py
+++ hello.py
@@ -1,5 +1,20 @@
 def main():
     print("Hello World")
 
+def print_factor(x):
+    print(f"{x}'s factors is ",end='')
+    for i in range(2, x):
+        # your code here
+        if x % i == 0:
+            print(i, end=' ')
+    print()
+
 if __name__ == '__main__':
     main()
+
+    x = 52633
+    print_factor(x)
+
+    l = [52633, 8137, 1024, 999]
+    for i in l:
+        print_factor(i)
